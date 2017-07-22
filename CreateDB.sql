fe0eae807c1c6a203040977830d72623f46a4c05
Daniel Shu <danielshu@Daniels-MacBook-Pro.local>
2017-07-22 12:05:31 -0700

    Adding create tables, small modifications

---
+++ CreateDB.sql
@@ -0,0 +1,114 @@
+-- Create Database
+
+DROP DATABASE IF EXISTS Job;
+CREATE DATABASE Job;
+USE Job;
+
+CREATE TABLE City (
+   id INT PRIMARY KEY,
+   name VARCHAR(25) NOT NULL,s
+   state CHAR(2) NOT NULL
+);
+
+CREATE TABLE Address (
+   id INT PRIMARY KEY,
+   addressNumber CHAR(5) NOT NULL,
+   street VARCHAR(40) NOT NULL,
+   zipcode CHAR(5) NOT NULL,
+   zipcodeExt CHAR(4),
+   cityId INT NOT NULL,
+   CONSTRAINT FKAddress_cityId FOREIGN KEY (cityId)
+    REFERENCES City(id)
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE Person (
+   id INT PRIMARY KEY,
+   firstName VARCHAR(30) NOT NULL,
+   lastName VARCHAR(30) NOT NULL,
+   salary DECIMAL(8, 2) UNSIGNED,
+   age TINYINT UNSIGNED,
+   gender ENUM('M', 'F', 'Other'),
+   addressId INT,
+   CONSTRAINT FKPerson_addressId FOREIGN KEY (addressId)
+    REFERENCES Address(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+   CONSTRAINT FKPerson_gender FOREIGN KEY (gender)
+    REFERENCES Gender(id)
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE Profession (
+   id INT PRIMARY KEY,
+   name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE Company (
+   id INT PRIMARY KEY,
+   name VARCHAR(40) NOT NULL,
+   cityID INT NOT NULL,
+   CONSTRAINT FKCompany_cityId FOREIGN KEY (cityId)
+    REFERENCES City(id)
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE Department (
+   id INT PRIMARY KEY,
+   name VARCHAR(50) NOT NULL,
+   companyId INT NOT NULL,
+   CONSTRAINT FKDepartment_companyId FOREIGN KEY (companyId)
+    REFERENCES Company(id)
+    ON UPDATE CASCADE,
+   UNIQUE KEY UKname_companyId(name, companyId)
+);
+
+CREATE TABLE Rating (
+   ratedId INT,
+   score TINYINT UNSIGNED NOT NULL,
+   raterId INT,
+   CONSTRAINT FKRating_personId FOREIGN KEY (personId)
+    REFERENCES Person(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+   CONSTRAINT FKRating_raterId FOREIGN KEY (raterId)
+    REFERENCES Rater(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+CREATE TABLE Division (
+   id INT PRIMARY KEY,
+   division VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE CompanyXProfession (
+   companyId INT,
+   professionId INT,
+   CONSTRAINT FKCompanyXProfession_companyId FOREIGN KEY (companyId)
+    REFERENCES Company(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+   CONSTRAINT FKCompanyXProfession_professionId FOREIGN KEY (professionId)
+    REFERENCES Profession(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+   UNIQUE KEY UKcompanyId_professionId(companyId, professionId)
+);
+
+CREATE TABLE PersonXProfession (
+   personId INT,
+   professionId INT,
+   hiredDate DATE NOT NULL,
+   division INT NOT NULL,
+   CONSTRAINT FKPersonXProfession_personId FOREIGN KEY (personId)
+    REFERENCES Person(id)
+    ON UPDATE CASCADE,
+   CONSTRAINT FKPersonXProfession_professionId FOREIGN KEY (professionId)
+    REFERENCES Profession(id)
+    ON UPDATE CASCADE,
+   CONSTRAINT FKPersonXProfession_division FOREIGN KEY (division)
+    REFERENCES Division(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
