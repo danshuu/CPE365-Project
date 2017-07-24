19b5186652e0d70648b5b4a93d04f8cd6e6cc0d1
cevenhub <cmnguyen110@gmail.com>
2017-07-24 14:24:51 -0700

    Created DataGen.java

---
+++ DataGen.java
@@ -0,0 +1,44 @@
+
+public class DataGen {
+
+	public static void genCity() {
+
+	}
+
+	public static void genHometownAddress() {
+
+	}
+
+	public static void genPerson() {
+
+	}
+
+	public static void genProfession() {
+
+	}
+
+	public static void genCompany() {
+
+	}
+
+	public static void genDepartment() {
+
+	}
+
+	public static void genEmployee() {
+
+	}
+
+	public static void genCompanyXProfession() {
+
+	}
+
+	public static void genRating() {
+		
+	}
+	
+	public static void main(String[] args) {
+	   Connection cnc = null;
+
+	}
+}
