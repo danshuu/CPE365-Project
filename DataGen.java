c3ef8fa1426bb87376b4b066247177fc035f9069
cevenhub <cmnguyen110@gmail.com>
2017-07-24 16:17:52 -0700

    Progress on genCity and genPerson

--- DataGen.java
+++ DataGen.java
@@ -1,15 +1,50 @@
 
 public class DataGen {
 
-	public static void genCity() {
+	private static final int NUM_PERSON = 120000;
+	private static final int NUM_CITY = 300;
 
+	public static void genCity(Connection cnc) {
+       ResultSet rst = null;
+       PreparedStatement pStm = null;
+
+       try {
+          pStm = cnc.prepareStatement("insert into" +
+           "City(name, state)" +
+           "values (?, ?)");
+
+          for (int i = 1; i < NUM_CITY; i++) {
+             pStm.setString(1, String.format("City%d", i));
+             pStm.setString(2, String.format("%02d", (i % 50) + 1));
+             pStm.executeUpdate();
+          }
+       }
+       finally {
+          closeEm(rst, pStm);
+       }
 	}
 
 	public static void genHometownAddress() {
 
 	}
 
-	public static void genPerson() {
+	public static void genPerson(Connection cnc) {
+	   ResultSet rst = null;
+       PreparedStatement pStm = null;
+
+       try {
+          pStm = cnc.prepareStatement("insert into" +
+           "Person(firstName, lastName, age, gender, hometownAddressId)" +
+           "values (?, ?, ?, ?, ?)");
+
+          for (int i = 1; i < NUM_PERSON; i++) {
+             pStm.setString(1, String.format("First%d", i));
+             pStm.setString(2, String.format("Last%d", i));
+          }
+       }
+       finally {
+          closeEm(rst, pStm);
+       }
 
 	}
 
@@ -39,6 +74,18 @@ public class DataGen {
 	
 	public static void main(String[] args) {
 	   Connection cnc = null;
+
+	   try {
+          cnc = DriverManager.getConnection(args[0], args[1], args[2]);
+
+          cnc
+	   }
+	   catch (SQLException err) {
+          System.out.println(err.getMessage());
+	   }
+	   finally {
+	   	  closeEm(cnc);
+	   }
 
 	}
 }
